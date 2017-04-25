7c1f196d942b50d1e2295975937065cdddc98aa7
ayusli127 <ayusli127@gmail.com>
2017-04-25 15:30:13 +0800

    android recyclerView 添加了动态和静态注册广播事件

--- RecyclerViewTest/app/src/main/java/com/example/administrator/recyclerviewtest/MainActivity.java
+++ RecyclerViewTest/app/src/main/java/com/example/administrator/recyclerviewtest/MainActivity.java
@@ -1,17 +1,26 @@
 package com.example.administrator.recyclerviewtest;
 
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.StaggeredGridLayoutManager;
 import android.widget.LinearLayout;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
+    private IntentFilter intentFilter;
+    private NetworkChangeReceiver networkChangeReceiver;
     private List<Fruit> fruitList = new ArrayList<>();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,6 +34,23 @@ public class MainActivity extends AppCompatActivity {
         recyclerView.setLayoutManager(layoutManager);
         FruitAdapter adapter =  new FruitAdapter(fruitList);
         recyclerView.setAdapter(adapter);
+        //动态注册广播
+        intentFilter =  new IntentFilter();
+        intentFilter.addAction("android.net.conn.CONNECTIVITY_CHANGE");
+        networkChangeReceiver =  new NetworkChangeReceiver();
+        registerReceiver(networkChangeReceiver,intentFilter);
+    }
+    class NetworkChangeReceiver extends BroadcastReceiver{
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            ConnectivityManager connectivityManager =(ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+            NetworkInfo networkInfo =  connectivityManager.getActiveNetworkInfo();
+            if(networkInfo != null && networkInfo.isAvailable()){
+                Toast.makeText(context,"network is available",Toast.LENGTH_SHORT).show();
+            }else{
+                Toast.makeText(context,"没有 wifi 啦",Toast.LENGTH_SHORT).show();
+            }
+        }
     }
     private void initFruits() {
         for (int i = 0; i < 2; i++) {
@@ -59,5 +85,11 @@ public class MainActivity extends AppCompatActivity {
             builder.append(name);
         }
         return  builder.toString();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        unregisterReceiver(networkChangeReceiver);
     }
 }
